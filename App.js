78055f292e12596e3e4d50ef5630d8f0be5d4799
Kabir Hasan <kabir.webdev@gmail.com>
2021-03-22 01:21:21 +0600

    This is App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import MatModal from "./components/MatModal";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Walah</h1>
+      <MatModal />
+    </div>
+  );
+}
+
+export default App;
