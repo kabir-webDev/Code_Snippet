ed5c43ed0659caaf9bc7d0277fee1ded9a9db2e8
Kabir Hasan <kabir.webdev@gmail.com>
2021-03-28 21:17:16 +0600

    Boom

--- firebaseData.js
+++ firebaseData.js
@@ -1,11 +1,61 @@
-// Getting data from firebase
-import { db } from "../firebase";
+import React, { useEffect, useState } from "react";
+import firebase from "../firebase";
+import { useForm } from "react-hook-form";
+import { v4 as uuidv4 } from "uuid";
 
-  const [posts, setPosts] = useState([]);
+export default function AddToFire() {
+  const { register, handleSubmit } = useForm();
+  const [user, setUser] = useState([]);
+
+  const ref = firebase.firestore().collection("users");
+
+  function getUsers() {
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setUser(items);
+    });
+  }
 
   useEffect(() => {
-    db.collection("users").onSnapshot((snapshot) => {
-      setPosts(snapshot.docs.map((doc) => doc.data()));
+    getUsers();
+  });
+
+  const onSubmit = (data) => {
+    ref.doc(uuidv4()).set({
+      name: data.name,
+      prof: data.prof,
+      market: data.market,
     });
-  }, []);
-  console.log(posts);
+  };
+
+  //   function addUser() {}
+
+  return (
+    <>
+      <div className="container">
+        <h2>
+          <u>Users</u>
+        </h2>
+        {user.map((u, index) => (
+          <div key={index}>
+            <h2>{u.name}</h2>
+            <h2>{u.prof}</h2>
+            <h2>{u.market}</h2>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name="name" placeholder="type your name" ref={register} />
+        <br />
+        <input name="prof" placeholder="type your profession" ref={register} />
+        <br />
+        <input name="market" placeholder="type the market" ref={register} />
+        <br />
+        <input type="submit" /> <br />
+      </form>
+    </>
+  );
+}
